368d630ee76271c3caba94b578486afbcc483639
nzooherd <nzooherd@gmail.com>
2021-10-30 22:22:25 +0800

    feat: open source

---
+++ pyzon/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2/6 21:44
+# @Author  : nzooherd
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+from pyzon.retry_utils import *
+from pyzon.supplier_utils import *
+from pyzon.request_utils import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyzon',
+      version='0.0.1',
+      description='Some python utils includes The Zen',
+      url='https://github.com/nzooherd/pyzon',
+      author='nzooherd',
+      author_email="nzooherd@gmail.com",
+      license='MIT',
+      packages=['pyzon'],
+      zip_safe=False)
